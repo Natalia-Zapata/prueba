f711a8248de0d1975e7b13f99590075b8030fe22
Natalia-Zapata <ndzapata980@misena.edu.co>
2023-02-21 11:33:12 -0500

    echo config project

---
+++ crudProductos.py
@@ -0,0 +1,4 @@
+from datetime import datetime
+from openpyxl import load_workbook
+Rut="C:\\Users\\SENA\\Desktop\\repo\\prueba\\Prueba repo\\productos.xlsx"
+Rut="C:\Users\SENA\Desktop\repo\prueba\Prueba repo\\productos.xlsx"
---
+++ crudTareas.py
@@ -0,0 +1,4 @@
+from datetime import datetime
+from openpyxl import load_workbook
+Rut="C:\\Users\\SENA\\Desktop\\repo\\prueba\\Prueba repo\\tarea.xlsx"
+Rut="C:\Users\SENA\Desktop\repo\prueba\Prueba repo\\tarea.xlsx"
